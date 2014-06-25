dcecacd5502b59d339413f0380b007c2e2cbd4c1
Hsin <shinsonh@gmail.com>
2014-06-25 13:57:14 -0400

    database 2nd setup

--- app.rb
+++ app.rb
@@ -8,3 +8,32 @@ use Rack::Flash, :sweep => true
 set :sessions => true
 
 set :database, "sqlite3:microblog.sqlite3"
+
+get '/' do 
+ erb :index
+	
+end
+
+get '/profile' do 
+ erb :profile 
+	
+end
+
+get '/edit' do 
+
+	erb :profile_edit
+	
+end
+
+get '/sign_up' do
+    erb :sign_up
+
+end
+
+get '/main' do 
+    erb :main
+	
+end
+
+
+
---
+++ db/migrate/20140625174610_create_followers.rb
@@ -0,0 +1,7 @@
+class CreateFollowers < ActiveRecord::Migration
+  def change
+  	create_table :followers do |t|
+  		t.integer :user_id
+  	end
+  end
+end
