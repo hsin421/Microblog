30cdc3bc5ef799095b62654192d7dea6e2fa68c8
Hsin <shinsonh@gmail.com>
2014-06-25 16:35:08 -0400

    paperclip experiment

--- app.rb
+++ app.rb
@@ -3,11 +3,16 @@ require 'sinatra/activerecord'
 require './model'
 require 'bundler/setup'
 require 'rack-flash'
+require 'paperclip'
+require 'datamapper'
+require 'dm-paperclip'
+
 enable :sessions
 use Rack::Flash
 set :sessions => true
 
 set :database, "sqlite3:microblog.sqlite3"
+DataMapper::setup(:default, "sqlite3:microblog.sqlite3")
 
 
 get '/' do
@@ -21,3 +26,31 @@ end
 get '/sign_up' do
 	erb :sign_up
 end
+
+#This part is for images/files upload from paperclip and datamapper gems
+#should do "gem install data_mapper" and "gem install paperclip" & "gem install dm-paperclip"
+
+def make_paperclip_mash(file_hash)
+  mash = Mash.new
+  mash['tempfile'] = file_hash[:tempfile]
+  mash['filename'] = file_hash[:filename]
+  mash['content_type'] = file_hash[:type]
+  mash['size'] = file_hash[:tempfile].size
+  mash
+end
+
+post '/upload' do
+  halt 409, "File seems to be emtpy" unless params[:file][:tempfile].size > 0
+  @resource = Resource.new(:file => make_paperclip_mash(params[:file]))
+  halt 409, flash[:error]="There were some errors processing your request...\n#{resource.errors.inspect}" unless @resource.save
+ 
+  erb :upload
+end
+ 
+get '/' do
+  erb :index
+end
+
+#Above == Paperclip/datamapper uploader
+
+
