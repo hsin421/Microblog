9b1d28044515a5027f31136f2bf164ed7c868e41
Hsin <shinsonh@gmail.com>
2014-06-26 14:10:20 -0400

    heroku up but rake not working yet

--- model.rb
+++ model.rb
@@ -19,15 +19,3 @@ class Following < ActiveRecord::Base
      belongs_to :user
 end
 
-# class Resource
-#   include DataMapper::Resource
-#   include Paperclip::Resource
-
-#   property :id,     Serial
-
-#   has_attached_file :file,
-#                     :url => "/system/:attachment/:id/:style/:basename.:extension",
-#                     :path => "/Users/Shin/Desktop/group/Microblog/Public/system/:attachment/:id/:style/:basename.:extension"
-# end
-
-# Resource.auto_migrate!
