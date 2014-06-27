956b4f8e1b0a4da148767d8f24ac528d0056fbd7
Mallory Bory <mallory.bory@gmail.com>
2014-06-27 14:30:17 -0400

    commiting before pushing

--- Public/myscript.js
+++ Public/myscript.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-a=11;
-$("#post_submit").click(function(){
+	a=11;
+	$("#post_submit").click(function(){
 
-//$(".post").animate({"top":"+=100px"}, 1000);
+	//$(".post").animate({"top":"+=100px"}, 1000);
 
-$(".post:last-child").remove();
+	$(".post:last-child").remove();
 
-$("#feed").prepend("<div class='post' id='f"+a+"'></div>");
-$("#f"+(a-1)).html($("#write").val());
-a = a+1;
+	$("#feed").prepend("<div class='post' id='f"+a+"'></div>");
+	$("#f"+(a-1)).html($("#write").val());
+	a = a+1;
 
-});
+	});
 
-
-
+	// turn border off of empty post divs
+	$('div.post:empty').hide();
 
 });
 
